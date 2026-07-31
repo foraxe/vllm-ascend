d6efcc0fccbd2d2bdd8a4b2720f2964e95089d66
yunzhi <ningyunxiao.nyx@antgroup.com>
2026-07-31 18:25:34 +0800

    fix(dsa-cp): clear packed block tables on device
    Signed-off-by: yunzhi <ningyunxiao.nyx@antgroup.com>

--- vllm_ascend/worker/block_table.py
+++ vllm_ascend/worker/block_table.py
@@ -353,7 +353,7 @@ class BlockTable:
         self.block_table.copy_to_gpu(num_reqs)
 
     def clear(self) -> None:
-        self.block_table.fill_(0)
+        self.block_table.gpu.fill_(0)
         self.block_table.cpu.fill_(0)
         if self.packed_global_slot_mapping is not None:
             self.packed_global_slot_mapping.fill_(PAD_SLOT_ID)
